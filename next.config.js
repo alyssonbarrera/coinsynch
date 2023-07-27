2c2f3e0d8e519e29f4d1150f1e1c3ced465496bb
Alysson Barrera <alyssonbarrera@outlook.com>
2023-07-26 22:50:43 -0300

    🐛 next.config setting

--- next.config.js
+++ next.config.js
@@ -14,4 +14,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  ...nextConfig,
+})
